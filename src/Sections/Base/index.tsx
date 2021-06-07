1488195a5ce26c9eb90d537e1d34ebdbf9c5f83a
Sudhan <sudhanplayz@gmail.com>
2021-06-07 09:09:12 +0000

    im closing gitpod so pushing changes

--- src/Sections/Base/index.tsx
+++ src/Sections/Base/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ShareButtons from "../../Components/ShareButtons"
 import style from "./style.module.sass"
 
 const Base = () => {
@@ -6,6 +7,7 @@ const Base = () => {
         <div className={style.TextContainer}>
             <h1 className={style.Header}>Let's Change <span>#IndianEducationSystem</span></h1>
             <p className={style.SubHeader}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+            <ShareButtons/>
         </div>
     </div>
 }
